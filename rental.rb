41e67c4f2fbf9d0f0a9c9ff0ba72c999cf9125a2
Glauber <glauber.bannwart@gmail.com>
2023-09-16 16:25:39 -0300

    update book and rental

--- rental.rb
+++ rental.rb
@@ -1,8 +1,13 @@
 class Rental
-  attr_accessor :date, :person
+  attr_accessor :date, :person, :book
 
-  def initialize(date, person)
+  def initialize(date, person, book)
     @date = date
+    @person = person
     person.rentals << self
+
+    @book = book
+    # *The rental is adding itself to the @book achieving the 'belongs-to' association
+    book.rentals << self
   end
 end
