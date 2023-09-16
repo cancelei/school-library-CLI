f242eaba72502ae79c3b991ee2f1e27cac9128d1
Glauber <glauber.bannwart@gmail.com>
2023-09-16 14:14:38 -0300

    add classroom

---
+++ classroom.rb
@@ -0,0 +1,19 @@
+class Classroom
+  attr_accessor :label, :students
+
+  def initialize(label)
+    @label = label
+    @students = []
+  end
+
+  def add_student(student)
+    @students << student
+    student.classroom = self
+  end
+end
+
+# classroom = Classroom.new('Math')
+# student = Student.new('Max')
+# classroom.add_student(student)
+# puts classroom.students[0].name
+# puts student.classroom.label
